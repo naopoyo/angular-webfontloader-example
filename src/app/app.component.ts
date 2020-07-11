1fdbf7fcaddf54178222589ad5e4d783b070a106
naopoyo <5816375+naopoyo@users.noreply.github.com>
2020-07-11 20:57:22 +0900

    AppComponentのngOnInitでwebfontを読み込む

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import * as WebFont from 'webfontloader'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    WebFont.load({
+      google: {
+        families: ['Modak', 'M+PLUS+1p:400,900'],
+      },
+    })
+  }
 }
